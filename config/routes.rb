94b0cd78ab6d7078621998a0c633a98c9d2eb731
Emmanuel MARIE <emmanuel.marie@qlapoti.fr>
2020-05-26 18:18:26 +0200

    booking#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :proposals do
-      resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:show, :new, :create]
   end
 end
