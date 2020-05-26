05470136c3b3ab9e7f7e118f1a1609f30878eb13
Emmanuel MARIE <emmanuel.marie@qlapoti.fr>
2020-05-26 16:37:05 +0200

    create bookings_controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+  # index.html.erb
+  def index
+    @bookings = Booking.all
+  end
+  
+  def new
+    @proposal = Proposal.find(params[:proposal_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @proposal = Proposal.new(booking_params)
+    @booking.user = current_user
+    @booking.proposal = Proposal.find(params[:proposal_id])
+    if @booking.save
+      flash[:notice] = "Booking done !"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:reservation_status, :url_room, :chatroom, :user, :proposal)
+  end
+end
