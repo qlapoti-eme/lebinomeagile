6bce1be763dc1f1024997ebae54d73591d2b6baf
Frederique Berthier <frederique.berthier@gmail.com>
2020-05-26 15:24:43 +0200

    create controller proposals and views

---
+++ app/controllers/proposals_controller.rb
@@ -0,0 +1,50 @@
+class ProposalsController < ApplicationController
+  before_action :find_proposal, only: [:destroy, :update, :show]
+
+  def index
+    @proposals = Proposal.all
+  end
+
+  def show
+  end
+
+  def new
+    @proposal = Proposal.new
+  end
+
+  def create
+    @proposal = Proposal.new(proposal_params)
+    if @proposal.save!
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @proposal.update(proposal_params)
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+
+  end
+
+  def destroy
+     @proposal.destroy
+     redirect_to proposals_path
+  end
+
+
+private
+
+  def proposal_params
+    params.require(:proposal).permit(:name, :description, :start_time, :end_time)
+  end
+
+  def find_proposal
+     @proposal = Proposal.find(params[:id])
+  end
+
+
+end
