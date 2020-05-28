73de8c214f517d5d163407af4d8b7335189e8432
Yoann Vuillermet <yoann.vuillermet@gmail.com>
2020-05-28 16:54:55 +0200

    getting last changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
   def index
     @bookings = policy_scope(Booking).order(created_at: :desc)
   end
-  
+
   def new
     @booking = Booking.new
     authorize @booking
