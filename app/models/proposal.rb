4f14edbf84492bc55015429f734a3c2d8046c174
Emmanuel MARIE <emmanuel.marie@qlapoti.fr>
2020-06-01 16:41:54 +0200

    new data_model + seeds ok

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -3,6 +3,7 @@ class Proposal < ApplicationRecord
   belongs_to :secondary_category
   delegate :primary_category, to: :secondary_category, allow_nil: true
   has_one :booking, dependent: :destroy
+  has_many :slots, dependent: :destroy
   # Pour simple_form #new
   # attr_accessor :primary_category
 end
