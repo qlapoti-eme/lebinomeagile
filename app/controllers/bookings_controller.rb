fb6fe05a993369a7b1ba41f112d8e5dc030615a8
Emmanuel MARIE <emmanuel.marie@qlapoti.fr>
2020-06-02 14:53:47 +0200

    init twolio

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,9 +17,28 @@ class BookingsController < ApplicationController
     authorize @booking
     if @booking.save!
       flash[:notice] = "Booking done !"
+      create_room(@booking)
       redirect_to dashboards_path
     else
       render :new
     end
   end
+
+  private
+
+  def create_room(booking)
+    # 
+    # Your Account Sid and Auth Token from twilio.com/console
+    # DANGER! This is insecure. See http://twil.io/secure
+    account_sid = ENV["ACCOUNT_SID"]
+    auth_token = ENV["AUTH_TOKEN"]
+    # Download the helper library from https://www.twilio.com/docs/ruby/install
+
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    room = @client.video.rooms.create(unique_name: 'DailyStandup')
+
+    puts room
+  end
+
 end
